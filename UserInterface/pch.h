10cee4dc2f5ab333271b93c086a32fea363b6f29
Butcher Tibi <47886094+ButcherTibi@users.noreply.github.com>
2020-09-21 19:49:42 +0300

    buildable try 2

--- UserInterface/pch.h
+++ UserInterface/pch.h
@@ -7,12 +7,6 @@
 #undef max
 #include <wrl\client.h>
 
-// Vulkan
-#define VK_USE_PLATFORM_WIN32_KHR
-#include <vulkan/vulkan.h>
-#include <vulkan/vulkan_win32.h>
-#undef VK_USE_PLATFORM_WIN32_KHR
-
 // DirectX 11
 #include <d3d11_4.h>
 #include <dxgi1_6.h>
@@ -28,8 +22,8 @@
 #include <cmath>
 
 // GLM
-#include "glm\vec2.hpp"
-#include "glm\vec4.hpp"
+#include <glm\vec2.hpp>
+#include <glm\vec4.hpp>
 
 // Mine
 #include "ErrorStack.hpp"
